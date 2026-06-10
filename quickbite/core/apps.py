d58d8aae2f09eca77c9f83dba2cd7d76ee17c5ba
SwiftTe <masterg578585@gmail.com>
2026-06-10 16:48:35 +0545

    feat(core): add app configuration

---
+++ quickbite/core/apps.py
@@ -0,0 +1,11 @@
+from django.apps import AppConfig
+
+
+class CoreConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "core"
+
+    def ready(self):
+        from core.admin_registry import register_quickbite_admin
+
+        register_quickbite_admin()
