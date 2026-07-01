1ec8bd384147903840af0135a3f8cf0f8acd3a5d
Kdlskd <monilramjali5@gmail.com>
2026-07-01 16:43:12 +0545

    feat(sql): add database creation script

---
+++ quickbite/sql/01_create_database.sql
@@ -0,0 +1,26 @@
+-- QuickBite Database Schema
+-- PostgreSQL Database Creation Script
+-- This file is for reference - database already created in pgAdmin4
+
+-- Database: quickbite_db
+-- Owner: postgres
+-- Encoding: UTF8
+-- Connection Limit: -1
+
+-- CREATE DATABASE quickbite_db
+-- WITH
+--     OWNER = postgres
+--     ENCODING = 'UTF8'
+--     LOCALE_PROVIDER = 'libc'
+--     CONNECTION LIMIT = -1
+--     IS_TEMPLATE = False;
+
+-- Grant all privileges to postgres
+-- GRANT ALL PRIVILEGES ON DATABASE quickbite_db TO postgres;
+
+-- Connect to the database
+-- \c quickbite_db
+
+-- Create extensions if needed
+-- CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+-- CREATE EXTENSION IF NOT EXISTS "pgcrypto";
